22f07f4775dd7ae7565f26ddd96b2208b45ba5de
Robin Ting <civ2boss@gmail.com>
2016-12-09 17:13:24 -0600

    Initial Commit

---
+++ src/components/MemoryGame.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MemoryGame extends React.Component {
+  render() {
+    return(
+      <h1>Memory Game</h1>
+    )
+  }
+}
+
+export default MemoryGame;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MemoryGame from './components/MemoryGame';
+
+ReactDOM.render(
+  <MemoryGame />,
+  document.getElementById('root')
+);
